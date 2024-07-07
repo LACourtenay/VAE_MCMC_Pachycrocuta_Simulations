e72059a671251ef74800d62e2e6c01cfbd4ce5f5
Lloyd Courtenay <ladc1995@gmail.com>
2024-07-07 13:03:10 +0200

    Updates to README.md files, with changes to the R code to split code used for pre and post-processing into two separate files

---
+++ Code/R Code - analyse python output.R
@@ -0,0 +1,59 @@
+
+# libraries and functions ---------------------------------------------
+
+library(geomorph)
+library(ggplot2)
+library(GraphGMM)
+
+euclid <- function(point1, point2) {
+  return(sqrt(sum((point1 - point2)^2)))
+}
+
+depth_estimate <- function(point1, point2, point3) {
+  
+  a <- euclid(point1, point2)
+  b <- euclid(point2, point3)
+  c <- euclid(point3, point1)
+  
+  s <- (a + b + c) / 2
+  area <- sqrt(s * (s - a) * (s - b) * (s - c))
+  height <- (2 * area) / a
+  
+  return(height)
+  
+}
+
+# Analyse output of VAE and MCMC --------------------------------------------------------------
+
+simulated_pachycrocuta1 <- read.table("reconstructed_pachycrocuta1.txt", header = FALSE, sep = ",")
+simulated_pachycrocuta2 <- read.table("reconstructed_pachycrocuta2.txt", header = FALSE, sep = ",")
+simulated_pachycrocuta3 <- read.table("reconstructed_pachycrocuta3.txt", header = FALSE, sep = ",")
+simulated_pachycrocuta4 <- read.table("reconstructed_pachycrocuta4.txt", header = FALSE, sep = ",")
+simulated_pachycrocuta <- rbind(simulated_pachycrocuta1, simulated_pachycrocuta2, simulated_pachycrocuta3, simulated_pachycrocuta4)
+np1 <- nrow(simulated_pachycrocuta)
+simulated_pachycrocuta_tensor <- array(numeric(), dim = c(25, 3, 0))
+for (i in 1:np1) {
+  
+  landmarks_prima <- matrix(simulated_pachycrocuta[i,], nrow = 25, byrow = TRUE)
+  simulated_pachycrocuta_tensor <- abind::abind(simulated_pachycrocuta_tensor, landmarks_prima, along = 3)
+  
+}
+
+# calculated simulated properties of tooth pits 
+
+sim_pachy_length <- c()
+sim_pachy_width <- c()
+sim_pachy_depth <- c()
+
+for (i in 1:dim(simulated_pachycrocuta_tensor)[3]) {
+  
+  sim_pachy1_length <- c(sim_pachy1_length, euclid(simulated_pachycrocuta_tensor[1,,i],
+                                                   simulated_pachycrocuta_tensor[2,,i]))
+  sim_pachy1_width <- c(sim_pachy1_width, euclid(simulated_pachycrocuta_tensor[3,,i],
+                                                 simulated_pachycrocuta_tensor[4,,i]))
+  sim_pachy1_depth <- c(sim_pachy1_depth, depth_estimate(simulated_pachycrocuta_tensor[1,,i],
+                                                         simulated_pachycrocuta_tensor[2,,i],
+                                                         simulated_pachycrocuta_tensor[5,,i]))
+  
+}
+
--- README.md
+++ README.md
@@ -28,22 +28,38 @@ The present repository contains:
 * <b> Code </b>
   * <b>MCMC Model.py</b>
     * Python code to load the trained VAE model, encode the target pachycrocuta tooth pits in latent space, and then use and MCMC algorithm to sample from the latent distribution. Afterwards these sampled latent coordiantes are used as input to the generator to simulate new tooth pits.
-  * <b>R Code.R</b>
+  * <b>R Code - GMM data.R</b>
     * R Code to load the landmark coordinates, superimpose them with the coordiantes of tooth pits left by Pachycrocuta brevirostris on the femur of a Hippo from FN3, and then export these superimposed Procrustes form coordiantes for modelling with the python code
     * This R Code also has a simple number of lines to load the simulated data and extract length, width and depth values from these simualted pits.
     * The R Code contains the landmark coordiantes obtained from the Hippo femur, but we do not have permission to share the reference landmark coordinates of modern carnivore species, because they come from other publications. These coordinates however are open access and have already been published. Consult the original publications for details on how to access them.
   * <b>VAE Model.py</b>
       * Python code that was originally used to define and train the Variational Autoencoder. Weights from the trained encoder are included in the Trained VAE Weights folder.
 
+Files need to be run in the following order: <b>R Code - GMM data.R</b>, <b>VAE Model.py</b>, <b>MCMC Model.py</b> and finally <b>R Code - analyse python output.R</b>.
+
 --------------------------------------------------------
 
 ## <b> System Requirements for Deep Learning </b>
 
+All code was run in an Anaconda (v.23.3.1) environment
+
 * Python
-    * Version 3.0 or higher
-* Tensorflow
-    * Version 2.0 or higher
-* Numpy
+  * Python library version 3.10.9
+  * Tensorflow
+    * Python library version 2.12.0
+  * Numpy
+    * Python library version 1.22.0
+* R
+  * Version 4.3.0
+  * geomorph
+    * R library version 4.0.5
+  * abind
+    * R library version 1.4.0
+  * GraphGMM
+    * R library version 1.0
+      * Available from https://github.com/LACourtenay/GraphGMM
+  * ggplot2 
+    * R library version 3.4.3
 
 --------------------------------------------------------
 
